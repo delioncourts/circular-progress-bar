bdcb7ddc0ac1f9371a43f86b3a331f1a62df4400
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2024-03-25 00:50:30 +0300

    feat: stylization, hide and animation

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const round = document.querySelector('.round');
+const spinner = document.querySelector('.spinner');
+
+const hide = () => {
+    round.classList.toggle("round__hide");
+};
+
+const spin = () => {
+    spinner.classList.toggle("round__spin");
+};
+
+
+/*const animate = () => {
+    round.classList.toggle("round__hide");
+};*/
