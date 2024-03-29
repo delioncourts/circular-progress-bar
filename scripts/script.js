4eff979cd1db3db0712324f747b31fb6989a43ba
Veronika Koksharova <91394025+delioncourts@users.noreply.github.com>
2024-03-29 12:54:15 +0300

    fix: value from input filled, small fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 const round = document.querySelector('.round');
 const spinner = document.querySelector('.spinner');
-const value = document.querySelector('.value');
+const input = document.querySelector('.value');
 
 const hide = () => {
   spinner.classList.toggle('round__hide');
@@ -10,8 +10,15 @@ const spin = () => {
   spinner.classList.toggle('round__spin');
 };
 
-const onChange = (evt) => {
-  console.log(this.value);
-};
+const filledChange = () => {
+  const inputValue = document.querySelector('.value').value;
+  const filledCircle = document.querySelector('.filled__circle');
 
-value.addEventListener('input', onChange, false);
+  const percentage = Math.min(1, Math.max(0, inputValue / 100));
+
+  // длина синей обводки в зависимости от введенного процента
+  const dashOffset = 439.6 - (percentage * 439.6); 
+  filledCircle.style.strokeDashoffset = dashOffset + 'px';
+}
+
+input.addEventListener('input', filledChange);
